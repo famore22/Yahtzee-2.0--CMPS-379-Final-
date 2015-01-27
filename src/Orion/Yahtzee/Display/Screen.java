36d8c7d93522e8428e23387fc475ddecbb703e0b
Felipe More <famore22@gmail.com>
2015-01-27 14:06:51 -0800

    Update Screen.java
    I think we should add a Rules screen for beginners and as a refresher for those who need it

--- src/Orion/Yahtzee/Display/Screen.java
+++ src/Orion/Yahtzee/Display/Screen.java
@@ -9,15 +9,22 @@ public class Screen extends Applet implements ActionListener{
 	Button
 			btnStart = new Button("Start"),
 			btnSettings = new Button("Settings");
+			//<Felipe Comment> I think we should add a rules screen for beginners and for those
+			//that need a refresher on the rules
+			btnRules = new Button("Rules");
 	
 	//Displays the title menu
 	public void menu() {
 		yahtzee = this.getImage(this.getCodeBase(), "yahtzee.jpg");
 		btnStart.setLocation(300, 200); btnStart.setSize(50, 30); add(btnStart);
 		btnSettings.setLocation(300, 250); btnSettings.setSize(50, 30); add(btnSettings);
+		//<Felipe Comment> where I think the location
+		btnRules.setLocation(300,300); btnRules.setSize(50,30); add(btnRules);
+		
 		
 		btnStart.addActionListener(this);
 		btnSettings.addActionListener(this);
+		btnRules.addActionListener(this);
 	}
 
 	Choice
